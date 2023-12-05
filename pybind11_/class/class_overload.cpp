754572993e982c06071c2db759269552b0918c75
duanmengmeng <dcdmmcomeon@gmail.com>
2023-12-06 01:01:08 +0800

    多个函数重载的方式学习

--- pybind11_/class/class_overload.cpp
+++ pybind11_/class/class_overload.cpp
@@ -7,6 +7,9 @@ namespace py = pybind11;
 struct Pet {
     Pet(const std::string &name, int age) : name(name), age(age) { }
 
+    Pet(const std::string &name, int age, int price) : name(name), age(age), price(price) { }
+
+
     void set(int age_) {
         std::cout << "call void set(int age_)" << std::endl;
         age = age_;
@@ -18,15 +21,19 @@ struct Pet {
 
     std::string name;
     int age;
+    int price = 100;
 };
 
 PYBIND11_MODULE(class_overload, m) {
     py::class_<Pet>(m, "Pet")
+            // 函数重载
             .def(py::init<const std::string &, int>())
+            .def(py::init<const std::string &, int, int>())
             // 函数重载
             .def("set", py::overload_cast<int>(&Pet::set), "Set the pet's age")
             .def("set", py::overload_cast<const std::string &>(&Pet::set), "Set the pet's name")
 
             .def_readwrite("name", &Pet::name)
-            .def_readwrite("age", &Pet::age);
+            .def_readwrite("age", &Pet::age)
+            .def_readwrite("price", &Pet::price);
 }
--- pybind11_/class/pytest/class_overload/test.py
+++ pybind11_/class/pytest/class_overload/test.py
@@ -5,8 +5,14 @@ import class_overload
 c = class_overload.Pet("dc", 28)
 print(c.name)
 print(c.age)
+print(c.price)
 # 原生python不支持函数重载
 c.set("dmm") # call void set(const std::string &name_)
 c.set(1) # call void set(int age_)
 print(c.name)
 print(c.age)
+
+c1 = class_overload.Pet("xiaoming", 3, 1000)
+print(c1.name)
+print(c1.age)
+print(c1.price)
