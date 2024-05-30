c37b32aba23089b339f23af9cf47401081e3993f
duanmengmeng <dcdmmcomeon@gmail.com>
2024-05-30 16:54:45 +0800

    no time no time

---
+++ rust_pyo3/func/src/lib.rs
@@ -0,0 +1,19 @@
+use pyo3::prelude::*;
+
+#[pyfunction]
+fn double(x: usize) -> usize {
+    x * 2
+}
+
+#[pyfunction]
+#[pyo3(name = "no_args")] // 修改函数在python中的名称(即调用从func.no_args_py()修改为func.no_args())
+fn no_args_py() -> usize {
+    42
+}
+
+#[pymodule]
+fn func(_py: Python, m: &Bound<PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(double, m)?)?;
+    m.add_function(wrap_pyfunction!(no_args_py, m)?)?;
+    Ok(())
+}
---
+++ rust_pyo3/func/src/py_test.py
@@ -0,0 +1,4 @@
+import func
+
+print(func.double(100))  # print->200
+print(func.no_args())  # 42
