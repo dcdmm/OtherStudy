da260952000b8751eaa27efa7b42da5554505145
duanmengmeng <dcdmmcomeon@gmail.com>
2024-06-03 14:17:59 +0800

    简单修正

--- rust_pyo3/other/src/lib.rs
+++ rust_pyo3/other/src/lib.rs
@@ -1,14 +1,11 @@
+#![allow(non_snake_case)]
+#![allow(warnings)]
+
+mod py_type;
+
 use pyo3::prelude::*;
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
-}
-
-/// A Python module implemented in Rust.
 #[pymodule]
-fn other(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+fn other(_py: Python, m: &Bound<PyModule>) -> PyResult<()> {
     Ok(())
 }
---
+++ rust_pyo3/other/src/py_type.rs
@@ -0,0 +1,31 @@
+use pyo3::prelude::*;
+use pyo3::types::*;
+
+// #[test]
+// fn PyString_test() {
+//     Python::with_gil(|py| {
+//         let s = PyString::new(py, "hello world");
+
+//     })
+// }
+
+#[test]
+fn PyBool_test() {}
+
+#[test]
+fn PyLong_test() {}
+
+#[test]
+fn PyFloat_test() {}
+
+#[test]
+fn PyList_test() {}
+
+#[test]
+fn PyTuple_test() {}
+
+#[test]
+fn PyDict_test() {}
+
+#[test]
+fn PySet_test() {}
