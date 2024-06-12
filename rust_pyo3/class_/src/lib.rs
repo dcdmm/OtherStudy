d5a67e36910c1bc8fb75bb7271142ba98157d5e8
duanmengmeng <dcdmmcomeon@gmail.com>
2024-06-12 16:16:55 +0800

    pyo3最后的学习

--- rust_pyo3/class_/src/lib.rs
+++ rust_pyo3/class_/src/lib.rs
@@ -1,6 +1,9 @@
+#![allow(non_snake_case)]
+#![allow(warnings)]
+
 use pyo3::prelude::*;
 
 #[pymodule]
-fn class_(_py: Python, m: &PyModule) -> PyResult<()> {
+fn class_(_py: Python, m: &Bound<PyModule>) -> PyResult<()> {
     Ok(())
 }
