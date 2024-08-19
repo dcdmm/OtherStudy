8869b641df75fdd8796180e8cc93a139aa5cf8d7
duanmengmeng <dcdmmcomeon@gmail.com>
2024-08-19 14:29:34 +0800

    一些完善项

--- CMake/c_add_compile_definitions0/main.cpp
+++ CMake/c_add_compile_definitions0/main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
 int main() {
-#ifdef DEBUG
-    std::cout << "******DEBUG*****\n";
+#ifdef DEBUG_PRINT
+    std::cout << "******DEBUG PRINT IMFOMATION*****\n";
 #endif
     for (int i = 0; i <= 5; ++i) {
         std::cout << "hello cmake!" << std::endl;
